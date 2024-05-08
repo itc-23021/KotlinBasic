1529e27ab298281ec7439ace8c38e32227e5c84a
s23021 <s23021@itc.it-college.ac.jp>
2024-05-08 16:59:56 +0900

    7章まで終わり

---
+++ src/main/kotlin/Chap03.kt
@@ -0,0 +1,8 @@
+package org.example
+fun main(args: Array<String>) {
+    //val message : String = "Hello World"
+    val message = "Hello World"
+    println(message)
+    println(message)
+    println(message)
+}
