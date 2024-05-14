799b2fe2787b6fad882d421e02b89b80028e6b37
itc-23021 <s23021@std.it-college.ac.jp>
2024-05-14 11:15:51 +0900

    13章終わり

---
+++ src/main/kotlin/Chap13.kt
@@ -0,0 +1,12 @@
+package org.exafun
+fun main(args: Array<String>) {
+    println("----13.2----")
+    val x = 5
+    for (i in 1..9) {
+        println("${x}かける${i}は、${x * i}です。")
+    }
+    println("----3.5----")
+    for (i in 800..1000 step 3)  {
+        println("${x}かける${i}は、${x * i}です。") }
+    }
+
