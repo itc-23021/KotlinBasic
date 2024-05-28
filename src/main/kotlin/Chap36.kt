ed9806bfb348cff0e73d66af56c9d057d87df2d8
itc-23021 <s23021@std.it-college.ac.jp>
2024-05-28 09:37:52 +0900

    36章終わり

---
+++ src/main/kotlin/Chap36.kt
@@ -0,0 +1,52 @@
+package  com.example.chap36
+
+fun main(args: Array<String>) {
+    run {
+        println("----36.1----")
+        val p = Person1()
+        p.greet()
+    }
+    run {
+        println("----36.2----")
+        val p = Person2()
+        p.greet()
+        p.greet2()
+
+
+    }
+    run {  }
+}
+
+class Person1 {
+    var name = "名無しさん"
+    var age = 0
+    fun greet() {
+        println("${name}です。${age}才です。")
+    }
+}
+class Person2 {
+    var name = "名無しさん"
+    var age = 0
+    fun greet() {
+        val name = "ヒロシ"
+        val  age = 46
+        println("${name}です。${age}です。")
+    }
+    fun greet2() {
+        println("${name}です。${age}です。")
+    }
+}
+class Person3 {
+    var name = ""
+    var age = 0
+
+    fun setNameAndAge(name: String, age: Int) {
+        this.name = name
+        this.age = age
+    }
+
+    fun greet() {
+        println("${name}です。${age}才です。")
+    }
+}
+
